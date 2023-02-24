3618a110a93f10d8cbcaff1370bf7d3ec641d920
Melendrez326 <125102352+Melendrez326@users.noreply.github.com>
2023-02-23 23:19:43 -0600

    tareas 23
    trareas 20203 23 02

---
+++ do-while.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cout<< "Ingrese un limitante"<<endl;
+    cin >> y;
+    do {
+        
+    cout <<  "Ingrese un numero ";
+        cin>> x;
+         }
+    while(x<= y);
+   
+    return 0;
+}
---
+++ while1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+  
+    cout<< "Ingrese un limitante"<<endl;
+    cin >> y;
+    
+   while(x<= y)
+    {
+   	cout <<  "Ingrese un numero ";
+    cin>> x;
+   }  
+    return 0;
+}
